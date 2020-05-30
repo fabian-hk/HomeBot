76b321002eb961097dc7b7b29948b302be3a4638
Fabian Hauck <hauckfabian@gmail.com>
2020-05-30 13:44:34 +0200

    add favicon and title to HTML page

--- main/website/index.php
+++ main/website/index.php
@@ -21,6 +21,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
 
         <link rel="stylesheet" href="css/main.css" />
+
+        <title>Home Bot</title>
+        <link rel="icon" type="image/png" href="favicon.png" />
     </head>
     <body style="margin: 20px">
         <div>
